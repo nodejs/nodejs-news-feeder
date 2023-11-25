70034ffb32894e8dd10bab31dc6dac9ce118dcb0
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-11-25 22:44:31 +0100

    chore: linting

--- scripts/build-website.js
+++ scripts/build-website.js
@@ -1,24 +1,24 @@
 import { getFeedContent, overwriteWebsiteContent, getWebsiteTemplate } from '../utils/index.js'
 import ejs from 'ejs'
-import { XMLValidator, XMLParser } from 'fast-xml-parser';
+import { XMLValidator, XMLParser } from 'fast-xml-parser'
 
-const parser = new XMLParser();
+const parser = new XMLParser()
 
-// @Ulises: Remove when @ulisesgascon/rss-feed-parser is fixed. 
+// @Ulises: Remove when @ulisesgascon/rss-feed-parser is fixed.
 // @see: https://github.com/UlisesGascon/micro-utilities/issues/24
-function rssParse(rssContent) {
-  const result = XMLValidator.validate(rssContent);
+function rssParse (rssContent) {
+  const result = XMLValidator.validate(rssContent)
   if (result.err !== undefined) {
-    throw new Error('XML is invalid');
+    throw new Error('XML is invalid')
   }
-  
-  const jObj = parser.parse(rssContent);
+
+  const jObj = parser.parse(rssContent)
 
   if (jObj?.rss?.channel === undefined) {
-    throw new Error('XML has not valid rss format');
+    throw new Error('XML has not valid rss format')
   }
 
-  const {item: items, ...metadata} = jObj.rss.channel;
+  const { item: items, ...metadata } = jObj.rss.channel
 
   return {
     metadata,
@@ -29,5 +29,5 @@ function rssParse(rssContent) {
 const template = getWebsiteTemplate()
 const feed = getFeedContent()
 const { metadata, items } = rssParse(feed)
-const html = ejs.render(template, { metadata, items });
+const html = ejs.render(template, { metadata, items })
 overwriteWebsiteContent(html)
