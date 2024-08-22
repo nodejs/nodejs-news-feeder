486e2263b989d8b7ada45b95157e55057aa1421c
Carl Vitullo <vcarl@users.noreply.github.com>
2024-08-22 16:42:02 -0400

    fix: Ensure posts are sorted chronologically
    PR-URL: https://github.com/nodejs/nodejs-news-feeder/pull/102

--- scripts/format.js
+++ scripts/format.js
@@ -1,6 +1,41 @@
+import ParseRss from 'rss-parser'
 import xmlFormat from 'xml-formatter'
-import { getFeedContent, overwriteFeedContent } from '../utils/index.js'
+import {
+  composeFeedItem,
+  getConfig,
+  getFeedContent,
+  overwriteFeedContent
+} from '../utils/index.js'
+
+const { breakDelimiter } = getConfig()
+
+const parser = new ParseRss()
 
 const xml = getFeedContent()
-const formattedXml = xmlFormat(xml, { indentation: '  ', collapseContent: true })
-overwriteFeedContent(formattedXml)
+
+parser.parseString(xml).then((parsedXml) => {
+  const sortedItems = parsedXml.items.sort(
+    (a, b) => new Date(b.isoDate) - new Date(a.isoDate)
+  )
+  const newXml = sortedItems
+    .map(({ title, link, pubDate, content, guid }) =>
+      composeFeedItem({
+        title,
+        description: `<![CDATA[${content}]]>`,
+        pubDate,
+        link,
+        guid
+      })
+    )
+    .join('')
+
+  const [before] = xml.split(breakDelimiter)
+  const updatedFeedContent = `${before}${breakDelimiter}${newXml}</channel></rss>`
+
+  const formattedXml = xmlFormat(updatedFeedContent, {
+    indentation: '  ',
+    collapseContent: true
+  })
+
+  overwriteFeedContent(formattedXml)
+})
